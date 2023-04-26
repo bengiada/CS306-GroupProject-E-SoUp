192f62cb22f77bd8f2f28caba67db1c7f56f6d66
bengiada <120020573+bengiada@users.noreply.github.com>
2023-04-26 22:53:32 +0300

    Add files via upload

--- SQL files/trigger.sql
+++ SQL files/trigger.sql
@@ -1,28 +1,32 @@
+DROP TRIGGER IF EXISTS fix_value_before_insert;
 delimiter //
 CREATE TRIGGER fix_value_before_insert 
 BEFORE INSERT 
 ON Access_to_Electricity
 FOR EACH ROW
 BEGIN  
-IF (percentage < 1) THEN UPDATE percentage SET percentage = 1;
-END IF;
-IF (percentage > 100) THEN UPDATE percentage  SET percentage = 100;
+IF NEW.percentage IS NOT NULL
+THEN
+	IF (NEW.percentage < 1) THEN SET NEW.percentage = 1;
+	END IF;
+	IF (NEW.percentage > 100) THEN  SET NEW.percentage = 100;
+	END IF;
 END IF;
 END//
 delimiter ;
 
+DROP TRIGGER IF EXISTS fix_value_before_update;
 delimiter //
 CREATE TRIGGER fix_value_before_update 
 BEFORE UPDATE ON Access_to_Electricity
 FOR EACH ROW
 BEGIN  
-IF percentage < 1 THEN UPDATE percentage SET percentage = 1;
-END IF; 
-IF percentage > 100 THEN UPDATE percentage SET percentage = 100;
+IF percentage IS NOT NULL
+THEN
+	IF percentage < 1 THEN UPDATE percentage SET percentage = 1;
+	END IF; 
+	IF percentage > 100 THEN UPDATE percentage SET percentage = 100;
+	END IF;
 END IF;
 END//
 delimiter ;
-
-
-
-
